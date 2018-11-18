bd7c558dc20e5064f0868ba573c0412745578dc6
fshatskiy <44747439+fshatskiy@users.noreply.github.com>
2018-11-18 13:35:21 +0100

    Update essaiLibre.h

--- essaiLibre.h
+++ essaiLibre.h
@@ -1,16 +1,16 @@
+/*
+PROJET
+2018
+*/
 #ifndef ESSAILIBRE_H
 #define ESSAILIBRE_H
 
 
-class essaiLibre
-{
-    public:
-        essaiLibre();
-        virtual ~essaiLibre();
-
-    protected:
-
-    private:
-};
+/*
+ * Crée le récapitulatif de l'essai libre
+ * @pre : cars not null
+ * @post : crée le fichier les informations 
+ */
+void generateRecapFilePractice();
 
 #endif // ESSAILIBRE_H
