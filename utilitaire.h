fa162dd0ea174a4eed9b47098d74a0a8e088503e
fshatskiy <44747439+fshatskiy@users.noreply.github.com>
2018-11-18 13:36:27 +0100

    Update utilitaire.h

--- utilitaire.h
+++ utilitaire.h
@@ -1,3 +1,7 @@
+/*
+PROJET
+2018
+*/
 #ifndef UTILITAIRE_H_INCLUDED
 #define UTILITAIRE_H_INCLUDED
 
@@ -5,72 +9,69 @@
 #include <sys/ipc.h>
 #include <sys/sem.h>
 
-StructCar cars[20] = {44,77,3,33,5,7,11,31,19,18,14,2,10,55,8,20,27,30,9,94};
-
 /*
- * Definition of the structure representing a car
+ * Définition de la strucutre de voiture
  */
 typedef struct structCar {
-	int number;           //Name of the car
-	double bestCircuit; //Best time on a circuit lap
-	double bestS1;      //Best time on the first sector
-	double bestS2;      //Best time on the second sector
-	double bestS3;      //Best time on the third sector
-	int numCircuit;     //Number of laps already done
-	double currTime;    //Time elapsed since the start of the race
-	double currCircuit; //Current time of the race
-	int pitStop;        /*number of times it has passed the stand (between 1 and 3) */
-	/*takes between 22 and 27 seconds */
-	int isOut;          //0 if the car still run, 1 if it's crash
+	int number;           //numéro de la voiture
+	double bestCircuit; //Meilleur temps du tour
+	double bestS1;      //Meilleur temps du secteur 1
+	double bestS2;      //Meilleur temps du secteur 2
+	double bestS3;      //Meilleur temps du secteur 3
+	double currTime;    //Temps lancé depuis le début de la course
+	double currCircuit; //Temps actuel de la course //PAS BESOIN ?
+	int nbrOfStands;        /*nombre de fois de passage au stand (entre 1 et 3) */
+	/*prend entre 22 et 27 secondes */
+	int isOut;          //retourne 1 si crash
 }structCar;
 
 /*
- * Initialize a car
- * @pre : i the index of the tab containing all the cars and the number of the car
- * @post : initialize car->number of the car indexed by i, the others are initialized at 0.
+ * Initialise la voiture
+ * @pre : i index du tableau centenant toutes les voitures et leurs nombres
+ * @post : initialise la voiture, numéro indexé par i, les autres sont initialisés à 0
  */
 void init(int i, int number);
 
 /*
- * Reset car's scores indexed by i
- * @pre : i the index of the tab containing all the cars
- * @post : all variable are reset except the car's number.
+ * Initialise les scores des voitures indexé par i
+ * @pre : i ,l'index du tableau contenant toutes les voitures
+ * @post : les variables sont initialisées sauf le numéro de voiture
  */
 void reset(int i);
 
 /*
- * This function generate a pit stop (based on a random function). i is the index of the car in the cars structure
+ * Génère un pitStop random. i est l'index de voiture de la structure
  * @pre : 0 <= i <= |cars|
- * @post : if pitStop, increments car[i].pitStop and returns time of that pitStop, otherwise returns 0.
+ * @post : si arrêt au pitStop, incrémente car[i].pitStop et retourne le temps de ce pitStop, sinon retourne 0
  */
 int pitStop(int i);
 
 /*
- * This function generate a random crash. index is the index of the car in cars.
+ * Génère un crash random. Index est l'index de la voiture dans "Voitures"
  * @pre : 0 <= index <= |cars|
- * @post : returns 1 if crash, 0 otherwise
+ * @post : retourne 1 si crash a eu lieu, 0 sinon
  */
 void crash(int index);
 
 /*
- * This function gets the maximum currentTime of the cars
- * @pre : structCar *cars exists
- * @post : returns the maximum currentTime of the cars
+ * Retourne le temps de chaque voiture 							????
+ * @pre : structCar *cars existe
+ * @post : retourne le temps max "currentTime" des voitures				????
  */
 double getCurrTime();
 
 /*
- * This function add time by position of the car for the race.
+ * Ajoute un certain temps dépendant de la position de la voiture au Start de la course
  * @pre:/
- * @post: returns nothing. Just add time to the cars.
+ * @post: retourne void. Ajoute le temps aux voitures pa rapport au placement
  */
 void addTimeByPosition();
 
 /*
- * This functions sets the out variables of the cars that can"t compete in q
+ * Modifie la variable "out" à 0 des voitures qui ne peuvent pas aller en qualif
  * to 1.
  * @pre: q==2||q==3
- * @post: returns nothing. out has been modified for the right cars
+ * @post: retourne void. "out" a été modifié pour les voitures concernées
  */
 void setOut(int q);
 
