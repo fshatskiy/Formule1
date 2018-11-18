d65913d61f2c9940a1afd031ac32f798d4f1870c
fshatskiy <44747439+fshatskiy@users.noreply.github.com>
2018-11-18 13:35:41 +0100

    Update qualif.h

--- qualif.h
+++ qualif.h
@@ -1,6 +1,18 @@
+/*
+PROJET
+2018
+*/
 #ifndef QUALIF_H_INCLUDED
 #define QUALIF_H_INCLUDED
 
+#include "utilitaire.h"
 
 
+
+/*
+ * Trie la table de voitures du base de bestCircuit
+ * @pre : carsQualif not null, sizeArrayCars==20 || sizeArrayCars==15 || sizeArrayCars==10
+ * @post : modification de carsQualif
+ */
+void sortCars(structCar carsQualif[], int sizeArrayCars);
 #endif // QUALIF_H_INCLUDED
