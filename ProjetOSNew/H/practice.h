013c8769be434f5e2cdbdbc8430ffb90844ee7d6
fshatskiy <44747439+fshatskiy@users.noreply.github.com>
2018-12-27 12:58:44 +0100

    Update practice.h

--- ProjetOSNew/H/practice.h
+++ ProjetOSNew/H/practice.h
@@ -8,12 +8,19 @@
  * @pre : index of a car, p==1 || p==2 || p==3
  * @post : modify the values in the car structure
  */
+//A Remplacer le commentaire par
+
+/*
+ * Lance l'essai sur la voiture située dans cars[index]
+ * @pre : index de la car, essai==1 || essai==2 || essai==3 ---> essais
+ * @post : modifie les valeurs de la strucutre car
+ */
 void practice(int index, int p);
 
 /*
- * Creates the file with the recap of the practice
+ * Crée le récapitulatif de l'essai libre
  * @pre : cars not null
- * @post : creates a file with all the infos needed
+ * @post : crée le fichier les informations 
  */
 void generateRecapFilePractice();
 
