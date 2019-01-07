58779edbe6bea7ce902b338b832494bed5118f8e
fshatskiy <44747439+fshatskiy@users.noreply.github.com>
2019-01-07 21:40:03 +0100

    Update qualification.h

--- qualification.h
+++ qualification.h
@@ -5,21 +5,21 @@
 
 /*
  * Trie la table de voitures du base de bestCircuit
- * @pre : voituresQualif not null, sizeArrayCars==20 || sizeArrayCars==15 || sizeArrayCars==10
+ * @pre : voituresQualif pas null, sizeArrayCars==20 || sizeArrayCars==15 || sizeArrayCars==10
  * @post : modification de voituresQualif
  */
 void triVoitures(structTuture voituresQualif[], int sizeArrayVoitures);
 
 /*
  * Construction de la grille de départ
- * @pre : voituresQualif not null, q==1 || q==2 || q==3
+ * @pre : voituresQualif pas null, q==1 || q==2 || q==3
  * @post : modification de la var startPosition
  */
 void buildStartPosition(structTuture voituresQualif[], int q);
 
 /*
  * Lance la qualif sur une voiture
- * @pre : tuture referenced by the index is not null, q==1 || q==2 || q==3
+ * @pre : tuture référencé par l'index pas null, q==1 || q==2 || q==3
  * @post : modification des valeurs dans la structure de la voiture
  */
 void qualif(int index, int q);
